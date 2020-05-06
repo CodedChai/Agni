4d56d24aed6488d02f93a6f863f85e615ae95a0c
Connor Hasting <guardianx515@gmail.com>
2020-05-05 19:50:56 -0500

    Added GPU support, started to rework the code into being much more readable and maintainable

---
+++ Agni/AgniMonitor.cs
@@ -0,0 +1,49 @@
+﻿using OpenHardwareMonitor.Hardware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agni
+{
+    class AgniMonitor
+    {
+        private void GetHardwareTemperature(IHardware hardware)
+        {
+            for (int j = 0; j < hardware.Sensors.Length; j++)
+            {
+                if (hardware.Sensors[j].SensorType == SensorType.Temperature)
+                {
+                    Console.WriteLine(hardware.Sensors[j].Name + ":" + hardware.Sensors[j].Value.ToString() + "\r");
+                }
+            }
+        }
+
+        public void GetSystemInfo()
+        {
+            HardwareUpdateVisitor updateVisitor = new HardwareUpdateVisitor();
+            Computer computer = new Computer();
+            computer.Open();
+            computer.CPUEnabled = true;
+            computer.GPUEnabled = true;
+            computer.Accept(updateVisitor);
+            for (int i = 0; i < computer.Hardware.Length; i++)
+            {
+                if (computer.Hardware[i].HardwareType == HardwareType.CPU)
+                {
+                    GetHardwareTemperature(computer.Hardware[i]);
+                }
+                else if (computer.Hardware[i].HardwareType == HardwareType.GpuNvidia)
+                {
+                    GetHardwareTemperature(computer.Hardware[i]);
+                }
+                else if (computer.Hardware[i].HardwareType == HardwareType.GpuAti)
+                {
+                    GetHardwareTemperature(computer.Hardware[i]);
+                }
+            }
+            computer.Close();
+        }
+    }
+}
--- Agni/Program.cs
+++ Agni/Program.cs
@@ -1,58 +1,17 @@
 ﻿using OpenHardwareMonitor.Hardware;
 using System;
 
-namespace Agni2
+namespace Agni
 {
-    internal class Program
+    class Program
     {
-        public class UpdateVisitor : IVisitor
-        {
-            public void VisitComputer(IComputer computer)
-            {
-                computer.Traverse(this);
-            }
-
-            public void VisitHardware(IHardware hardware)
-            {
-                hardware.Update();
-                foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
-            }
-
-            public void VisitSensor(ISensor sensor)
-            {
-            }
-
-            public void VisitParameter(IParameter parameter)
-            {
-            }
-        }
-
-        private static void GetSystemInfo()
-        {
-            UpdateVisitor updateVisitor = new UpdateVisitor();
-            Computer computer = new Computer();
-            computer.Open();
-            computer.CPUEnabled = true;
-            computer.Accept(updateVisitor);
-            for (int i = 0; i < computer.Hardware.Length; i++)
-            {
-                if (computer.Hardware[i].HardwareType == HardwareType.CPU)
-                {
-                    for (int j = 0; j < computer.Hardware[i].Sensors.Length; j++)
-                    {
-                        if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Temperature)
-                            Console.WriteLine(computer.Hardware[i].Sensors[j].Name + ":" + computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
-                    }
-                }
-            }
-            computer.Close();
-        }
 
         private static void Main(string[] args)
         {
+            AgniMonitor agniMonitor = new AgniMonitor();
             while (true)
             {
-                GetSystemInfo();
+                agniMonitor.GetSystemInfo();
             }
         }
     }
