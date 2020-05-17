4fad89046f9966e75b326e4f4d958721ad5a03da
Connor Hasting <guardianx515@gmail.com>
2020-05-17 16:42:57 -0500

    Finishing up the logic for finding the arduino

--- Agni/Program.cs
+++ Agni/Program.cs
@@ -44,59 +44,32 @@ namespace Agni
 
         private static SerialPort InstantiatePort(string portName)
         {
-            return new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+            return new SerialPort(portName, 9600, Parity.None, 8, StopBits.One);
         }
 
         private static async Task ReadSerialBytesAsync (SerialPort port)
         {
-
-            var cancellation = new CancellationTokenSource(300);
-            cancellation.CancelAfter(300);
-            CancellationToken token = cancellation.Token;
 
             if (!port.IsOpen)
             {
                 port.Open();
                 Console.WriteLine("Opening port " + port.PortName);
             }
-
             var reader = PipeReader.Create(port.BaseStream);
 
             Console.WriteLine("Is the port open? " + port.IsOpen);
 
-            while (!token.IsCancellationRequested && port.IsOpen)
+            using(CancellationTokenSource cancellation = new CancellationTokenSource(1000))
             {
-                try
-                {
-                    Console.WriteLine("We are about to try to read stuff...");
-                    ReadResult readResult = reader.ReadAsync(token).GetAwaiter().GetResult();
-                    Console.WriteLine("Reading stuff " + readResult.Buffer + " " + readResult.ToString());
-                    // find and handle packets
-                    // Normally wrapped in a handle-method and a while to allow processing of several packets at once 
-                    // while(HandleIncoming(result))
-                    // {
-                    Console.WriteLine(readResult.Buffer.PositionOf((byte)'~').HasValue);
+                Console.WriteLine("We are about to try to read stuff...");
+                cancellation.Token.Register(() => reader.CompleteAsync());
+                var readResult = await reader.ReadAsync(cancellation.Token);
 
-                    readResult.Buffer.Slice(10); // Moves Buffer.Start to position 10, which we use later to advance the reader
-                                                 // }
-
-                    Console.WriteLine(readResult.Buffer.ToString());
-
-                    // Tell the PipeReader how much of the buffer we have consumed. This will "free" that part of the buffer
-                    reader.AdvanceTo(readResult.Buffer.Start, readResult.Buffer.End);
-
-                    // Stop reading if there's no more data coming
-                    if (readResult.IsCompleted)
-                    {
-                        break;
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Error in ReadSerialBytesAsync: " + ex.ToString());
-                    throw ex;
-                }
+                Console.WriteLine("Reading stuff " + readResult.Buffer + " " + readResult.ToString());
+      
+                Console.WriteLine("Is this one of the arduinos: " + readResult.Buffer.PositionOf((byte)'~').HasValue);
             }
+
             if (port.IsOpen)
             {
                 port.Close();
