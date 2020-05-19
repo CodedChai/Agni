dc9ce57d611a7302acfd055840c210bc4654f53a
Connor Hasting <guardianx515@gmail.com>
2020-05-18 19:01:37 -0500

    Created an installer!

--- Agni/Properties/AssemblyInfo.cs
+++ Agni/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Agni")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Send CPU & GPU Temps to Arduinos")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("CodedChai")]
 [assembly: AssemblyProduct("Agni")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
